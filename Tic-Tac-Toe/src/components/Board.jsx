c76efaf3d9fec4c735f391af3e07e96400e3520b
sahasourav17 <sahasourav1170@gmail.com>
2023-03-14 14:40:21 +0600

    handle players move

--- Tic-Tac-Toe/src/components/Board.jsx
+++ Tic-Tac-Toe/src/components/Board.jsx
@@ -2,27 +2,32 @@ import React, { useState } from "react";
 import Square from "./Square";
 import styles from "./Board.module.css";
 const Board = () => {
-  const [square, setSquare] = useState(Array(9).fill(null));
-  const handleClick = (index) => {
-    console.log(index);
+  const [squareState, setSquareState] = useState(Array(9).fill(null));
+  const [isXMove, setIsXMove] = useState(true);
+
+  const handleClick = (idx) => {
+    const moveState = [...squareState];
+    moveState[idx] = isXMove ? "X" : "0";
+    setSquareState(moveState);
+    setIsXMove(!isXMove);
   };
   return (
     <>
       <div className={styles.boardContainer}>
         <div className={styles.boardRow}>
-          <Square value={square[0]} onClick={() => handleClick(0)} />
-          <Square value={square[1]} onClick={() => handleClick(1)} />
-          <Square value={square[2]} onClick={() => handleClick(2)} />
+          <Square value={squareState[0]} onClick={() => handleClick(0)} />
+          <Square value={squareState[1]} onClick={() => handleClick(1)} />
+          <Square value={squareState[2]} onClick={() => handleClick(2)} />
         </div>
         <div className={styles.boardRow}>
-          <Square value={square[3]} onClick={() => handleClick(3)} />
-          <Square value={square[4]} onClick={() => handleClick(4)} />
-          <Square value={square[5]} onClick={() => handleClick(5)} />
+          <Square value={squareState[3]} onClick={() => handleClick(3)} />
+          <Square value={squareState[4]} onClick={() => handleClick(4)} />
+          <Square value={squareState[5]} onClick={() => handleClick(5)} />
         </div>
         <div className={styles.boardRow}>
-          <Square value={square[6]} onClick={() => handleClick(6)} />
-          <Square value={square[7]} onClick={() => handleClick(7)} />
-          <Square value={square[8]} onClick={() => handleClick(8)} />
+          <Square value={squareState[6]} onClick={() => handleClick(6)} />
+          <Square value={squareState[7]} onClick={() => handleClick(7)} />
+          <Square value={squareState[8]} onClick={() => handleClick(8)} />
         </div>
       </div>
     </>
